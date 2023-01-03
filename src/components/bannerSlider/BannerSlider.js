ebd32de610a98a9417254b5f5e5de5edbe2df6d8
ArtFED <temfedorenko@gmail.com>
2023-01-03 09:17:38 +0200

    add new comics slider

--- src/components/bannerSlider/BannerSlider.js
+++ src/components/bannerSlider/BannerSlider.js
@@ -5,39 +5,49 @@ import { ReactComponent as ArrowRightIcon } from "../../assets/icons/arrow-right
 import firstSlider from "../../assets/images/marvel-comics1.jpg";
 import secondSlider from "../../assets/images/marvel-comics2.jpg";
 import thirdSlider from "../../assets/images/marvel-comics3.jpg";
+import fourthSlider from "../../assets/images/marvel-comics4.jpg";
+import fifthSlider from "../../assets/images/marvel-comics5.jpg";
 import "swiper/css";
 
 import "./BannerSlider.scss";
 
 const BannerSlider = () => {
   return (
-    <div className="swiper__wrapper">
-      <div className="swiper-btn swiper-btn-prev">
-        <ArrowRightIcon className="arrow" />
+    <div className="banner-slider">
+      <div className="swiper__wrapper">
+        <div className="swiper-btn swiper-btn-prev">
+          <ArrowRightIcon className="arrow" />
+        </div>
+        <div className="swiper-btn swiper-btn-next">
+          <ArrowRightIcon />
+        </div>
+        <Swiper
+          navigation={{
+            nextEl: ".swiper-btn-next",
+            prevEl: ".swiper-btn-prev",
+            disabledClass: "swiper-btn-disabled",
+          }}
+          speed={500}
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={1}>
+          <SwiperSlide>
+            <img src={firstSlider} alt="comic" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={secondSlider} alt="comic" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={thirdSlider} alt="comic" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={fourthSlider} alt="comic" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={fifthSlider} alt="comic" />
+          </SwiperSlide>
+        </Swiper>
       </div>
-      <div className="swiper-btn swiper-btn-next">
-        <ArrowRightIcon />
-      </div>
-      <Swiper
-        navigation={{
-          nextEl: ".swiper-btn-next",
-          prevEl: ".swiper-btn-prev",
-          disabledClass: "swiper-btn-disabled",
-        }}
-        speed={500}
-        modules={[Navigation]}
-        spaceBetween={50}
-        slidesPerView={1}>
-        <SwiperSlide>
-          <img src={firstSlider} alt="comic" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={secondSlider} alt="comic" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={thirdSlider} alt="comic" />
-        </SwiperSlide>
-      </Swiper>
     </div>
   );
 };
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchComics } from "../../features/comics/comicsSlice";
 import BannerSlider from "../bannerSlider/BannerSlider";
+import NewComicsSlider from "../newComicsSlider/NewComicsSlider";
 
 const HomePage = () => {
-  return <BannerSlider />;
+  const comics = useSelector((state) => state.comics.comics);
+  const newestComics = comics
+    .slice()
+    .sort((prev, next) => +next.year - +prev.year)
+    .slice(0, 12);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!comics.length) {
+      dispatch(fetchComics());
+    }
+  }, []);
+  return (
+    <>
+      <BannerSlider />
+      <NewComicsSlider comics={newestComics} />
+    </>
+  );
 };
 
 export default HomePage;
